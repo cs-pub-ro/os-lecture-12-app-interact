c23290418916f8d5c12502bc1bcafa522f9859fb
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2026-01-24 16:13:03 +0200

    demos: Use simplified utils.h
    Do not call `log()` in utils.h header. That will cause adding another
    source code file in the build process, and it's cumbersome.
    
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

--- demos/utils/utils.h
+++ demos/utils/utils.h
@@ -1,36 +1,32 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
+/**
+ * SO, 2011
+ * Lab #3, Procese
+ *
+ * Useful structures/macros
+ */
 
-#ifndef UTILS_H_
-#define UTILS_H_ 1
+#ifndef LIN_UTILS_H_
+#define LIN_UTILS_H_	1
 
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "log/log.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define ERR(assertion, call_description)				\
-	do {								\
-		if (assertion)						\
-			log_error("%s: %s",				\
-				call_description, strerror(errno));	\
+#define ERR(call_description)				\
+	do {						\
+		fprintf(stderr, "(%s, %d): ",		\
+				__FILE__, __LINE__);	\
+			perror(call_description);	\
 	} while (0)
 
+/* useful macro for handling error codes */
 #define DIE(assertion, call_description)				\
 	do {								\
-		if (assertion)	{					\
-			log_fatal("%s: %s",				\
-				call_description, strerror(errno));	\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
 			exit(EXIT_FAILURE);				\
 		}							\
-	} while (0)
+	} while(0)
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif  /* UTILS_H_ */
